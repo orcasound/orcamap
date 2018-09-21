9b1584fccc1d9d18f61c60bba847f545adf66e5b
Christian P. Sarason <cpsarason@gmail.com>
2018-09-20 17:10:23 -0700

    refactoring of app with cleaner node setup. loads more to do here.

--- www/js/app.js
+++ www/js/app.js
@@ -14,391 +14,226 @@ var app = {
     '#profile-form submit': 'saveProfile'
   },
 
-  login: function(event) {
+  // Google Sheet API Stuff
+
+  googleSheetSetup: function googleSheetSetup() {
+  
+    var gSheetJSON = {
+      // Client ID and API key from the Developer Console
+      CLIENT_ID : '1074262146370-umh810j84cdk47ocqf0f28v65gdmriue.apps.googleusercontent.com',
+      API_KEY : 'AIzaSyC1stf2UedjvWn3C_rS4cC8Rhb4BwcjugA',
+
+      // Array of API discovery doc URLs for APIs used by the quickstart
+      DISCOVERY_DOCS : ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
+
+      // Authorization scopes required by the API; multiple scopes can be
+      // included, separated by spaces.
+      SCOPES : "https://www.googleapis.com/auth/spreadsheets",
+
+      // Spreadsheet ID and cell range for retrieval of obsType
+      WORKBOOKID : "1Ezv9Xqa_CmFR3kwEMUYCy1GMDOm1la_gzZsXUIsOy_E",
+      VISUALSHEET : "OrcaSoundVisualObs",
+      ACOUSTICSHEET : "OrcaSoundAcousticObs",
+      RANGE : "A2:I",
+
+      authorizeButton : document.getElementById('authorize-button'),
+      signoutButton : document.getElementById('signout-button')
+    }
+
+    return gSheetJSON;
+  },
+
+
+  // /**
+  //  *  On load, called to load the auth2 library and API client library.
+  //  */
+  // // *****SECTION: GOOGLE AUTH*******
+  // // TODO: This section should be moved to app.js
+
+  // handleClientLoad: function handleClientLoad() {
+  //   gapi.load('client:auth2', initClient);
+  // },
+
+  // /**
+  //  *  Initializes the API client library and sets up sign-in state
+  //  *  listeners.
+  //  */
+  // function initClient() {
+  //   gapi.client.init({
+  //     apiKey: API_KEY,
+  //     clientId: CLIENT_ID,
+  //     discoveryDocs: DISCOVERY_DOCS,
+  //     scope: SCOPES
+  //   }).then(function () {
+  //     // Listen for sign-in state changes.
+  //     gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+  //     // Handle the initial sign-in state.
+  //     updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+  //     authorizeButton.onclick = handleAuthClick;
+  //     signoutButton.onclick = handleSignoutClick;
+  //   });
+  // }
+
+  // /**
+  //  *  Called when the signed in status changes, to update the UI
+  //  *  appropriately. After a sign-in, the API is called.
+  //  */
+  // function updateSigninStatus(isSignedIn) {
+  //   if (isSignedIn) {
+  //     authorizeButton.style.display = 'none';
+  //     signoutButton.style.display = 'block';
+  //     listObs();
+  //   } else {
+  //     authorizeButton.style.display = 'block';
+  //     signoutButton.style.display = 'none';
+  //   }
+  // }
+
+  // /**
+  //  *  Sign in the user upon button click.
+  //  */
+  // function handleAuthClick(event) {
+  //   gapi.auth2.getAuthInstance().signIn();
+  // }
+
+  // /**
+  //  *  Sign out the user upon button click.
+  //  */
+  // function handleSignoutClick(event) {
+  //   gapi.auth2.getAuthInstance().signOut();
+  // }
+
+  // /**
+  //  * Append a pre element to the body containing the given message
+  //  * as its text node. Used to display the results of the API call.
+  //  *
+  //  * @param {string} message Text to be placed in pre element.
+  //  */
+  // function appendPre(message) {
+  //   var pre = document.getElementById('obsListing');
+  //   var textContent = document.createTextNode(message + '\n');
+  //   pre.appendChild(textContent);
+  // }
+  // // ***** END GOOGLE AUTH *******
+
+
+  authorizeSheet: function () {
+    console.log('stub of authorizing to google sheet');
+  },
+
+
+  loadTideStations: function (dataStoreType) {
+      console.log('in loadTideStations');
+      // Render the table
+      function renderTable(stations, selector) {
+          // Default stations to an empty array
+          stations = stations || [];
+
+          // Create the rows
+          var rows = stations.map(function (station) {
+              return '<tr>\n'
+                + '<td>' + station.name + '</td>\n'
+                + '<td>' + station.id + '</td>\n'
+                + '<td>' + station.lat + '</td>\n'
+                + '<td>' + station.lng + '</td>\n'
+                + '<td>' + (station.reference_id || '') + '</td>\n'
+                + '</tr>';
+          });
+
+          // Create the table
+          var html = '<table class="table table-striped">\n'
+            + '<thead>\n'
+            + '<tr>\n'
+            + '<th>Station Name</th>\n'
+            + '<th>ID</th>\n'
+            + '<th>Latitude</th>\n'
+            + '<th>Longitude</th>\n'
+            + '<th>Reference ID</th>\n'
+            + '</tr>\n'
+            + '</thead>\n'
+            + '<tbody>\n'
+            + rows.join('')
+            + '</tbody>\n'
+            + '</table>\n';
+
+          // Add the html to the page
+          $(selector).html(html);
+      }
+
+      // Load the stations
+      var store = Kinvey.DataStore.collection('NOAAStations', dataStoreType);
+      store.find()
+        .subscribe(function (stations) {
+            return renderTable(stations, '#NOAAStations-table')
+        });
+  },
+
+  uploadFile: function(event) {
+    // stub for uploading files to google sheet?? 
     // Prevent the form from being submitted
     event.preventDefault();
 
-    // Remove the login-error
-    $('#login-error').hide(0);
+    // Remove the upload-success and upload-error
+    $('#upload-success').hide(0);
+    $('#upload-error').hide(0);
+    $('#exif-data').hide(0);
 
-    // Get entered username and password values
-    var username = $('#username').val();
-    var password = $('#password').val();
+    // Get entered values
+    var file = $('#file')[0].files[0];
 
-    // Login to Kinvey
-    Kinvey.User.login(username, password)
-      .then(function() {
-        location.replace('/');
-      })
-      .catch(function(error) {
-        $('#login-error').html('<p>' + error.message + '</p>').show(0);
-      });
-    },
+    if (file) {
+      var filename = $('#filename').val();
+      var public = document.getElementById('public').checked;
+      var acloptions_json = {};
+      filename = filename || filename !== '' ? filename : file.name;
 
-    loginWithMIC: function(event) {
-      // Prevent the form from being submitted
-      event.preventDefault();
+        // this is where I think the EXIF scrape can come for uploading to Kinvey. 
+        // Really belongs as a pre-save file hook on Kinvey, though.
 
-      // Remove the login-error
-      $('#login-error').hide(0);
+      // Show progress
 
-      // Login with Mobile Identity Connect
-      Kinvey.User.loginWithMIC('<micRedirectUri>')
-        .then(function() {
-          location.replace('/');
-        })
-        .catch(function(error) {
-          $('#login-error').html('<p>' + error.message + '</p>').show(0);
-        });
-    },
+      $('#upload-progress').show(0);
 
-    signup: function(event) {
-      // Prevent the form from being submitted
-      event.preventDefault();
-
-      // Remove the signup-error
-      $('#signup-error').hide(0);
-
-      // Get entered values
-      var data = {
-        username: $('#username').val(),
-        password: $('#password').val(),
-        firstname: $('#firstname').val(),
-        lastname: $('#lastname').val()
-      };
-
-      // Login to Kinvey
-      Kinvey.User.signup(data)
-        .then(function() {
-          location.replace('/');
-        })
-        .catch(function(error) {
-          $('#signup-error').html('<p>' + error.message + '</p>').show(0);
-        });
-    },
-
-    loadBooks: function (dataStoreType) {
-      // TODO: add map-center to this function
-      // Render the table
-      function renderTable(books, selector) {
-        // Default books to an empty array
-        books = books || [];
-
-        // Create the rows
-        var rows = books.map(function(book) {
-          return '<tr>\n'
-            + '<td><a href="https://tidesandcurrents.noaa.gov/noaatidepredictions.html?id=' + book.id +'">' + book.id + '</td>\n'
-            + '<td>' + book.name + '</td>\n'
-            + '<td>' + book.lat + '</td>\n'
-            + '<td>' + book.lng + '</td>\n'
-            + '</tr>';
-        });
-
-        // Create the table
-        var html = '<table class="table table-striped">\n'
-          +   '<thead>\n'
-          +     '<tr>\n'
-          +       '<th>Station Name</th>\n'
-          +       '<th>ID</th>\n'
-          +       '<th>Latitude</th>\n'
-          +       '<th>Longitude</th>\n'
-          +     '</tr>\n'
-          +   '</thead>\n'
-          +   '<tbody>\n'
-          +     rows.join('')
-          +   '</tbody>\n'
-          + '</table>\n';
-
-        // Add the html to the page
-        $(selector).html(html);
-      }
-
-      // Load the books
-      var query = new Kinvey.Query();
-      query.ascending('lat');
-      query.ascending('lng');
       
+      //console.log($('#user-location-lat')[0].textContent);
+      //console.log($('#user-location-lng'));
 
-      var store = Kinvey.DataStore.collection('books', dataStoreType);
-      store.find(query)
-        .subscribe(function(books) {
-          return renderTable(books, '#books-table')
-        });
-    },
-
-    loadTideStations: function (dataStoreType) {
-        // Render the table
-        function renderTable(stations, selector) {
-            // Default stations to an empty array
-            stations = stations || [];
-
-            // Create the rows
-            var rows = stations.map(function (station) {
-                return '<tr>\n'
-                  + '<td>' + station.name + '</td>\n'
-                  + '<td>' + station.id + '</td>\n'
-                  + '<td>' + station.lat + '</td>\n'
-                  + '<td>' + station.lng + '</td>\n'
-                  + '<td>' + (station.reference_id || '') + '</td>\n'
-                  + '</tr>';
-            });
-
-            // Create the table
-            var html = '<table class="table table-striped">\n'
-              + '<thead>\n'
-              + '<tr>\n'
-              + '<th>Station Name</th>\n'
-              + '<th>ID</th>\n'
-              + '<th>Latitude</th>\n'
-              + '<th>Longitude</th>\n'
-              + '<th>Reference ID</th>\n'
-              + '</tr>\n'
-              + '</thead>\n'
-              + '<tbody>\n'
-              + rows.join('')
-              + '</tbody>\n'
-              + '</table>\n';
-
-            // Add the html to the page
-            $(selector).html(html);
-        }
-
-        // Load the stations
-        var store = Kinvey.DataStore.collection('NOAAStations', dataStoreType);
-        store.find()
-          .subscribe(function (stations) {
-              return renderTable(stations, '#NOAAStations-table')
-          });
-    },
-
-    loadFiles: function() {
-      // Render the table
-      function renderTable(files, selector) {
-        // Default files to an empty array
-        files = files || [];
-
-        // Create the rows
-        var rows = files.map(function(file) {
-          return '<tr>\n'
-            + '<td>' + file._filename + '</td>\n'
-            + '<td>' + file.mimeType + '</td>\n'
-            + '<td>' + file._public + '</td>\n'
-            + '<td><a target="_blank" href="' + file._downloadURL + '">Download URL</a></td>\n'
-            + '</tr>';
-        });
-
-        // Create the table
-        var html = '<table class="table table-striped">\n'
-          +   '<thead>\n'
-          +     '<tr>\n'
-          +       '<th>Filename</th>\n'
-          +       '<th>MIME Type</th>\n'
-          +       '<th>Public</th>\n'
-          +       '<th>Url</th>\n'
-          +     '</tr>\n'
-          +   '</thead>\n'
-          +   '<tbody>\n'
-          +     rows.join('')
-          +   '</tbody>\n'
-          + '</table>\n';
-
-        // Add the html to the page
-        $(selector).html(html);
-      }
-
-      // Load the files
-      Kinvey.Files.find()
-        .then(function(files) {
-          return renderTable(files, '#files-table')
-        });
-    },
-
-    loadGeoFiles: function (bbox_ll,bbox_ur, markerJSON) {
-        // Render the table
-        function renderTable(files, selector) {
-            // Default files to an empty array
-            files = files || [];
-          
-            
-            // Create the rows
-            var rows = files.map(function (file) {
-                    
-                markerJSON.features.push({
-                    type: 'Feature',
-                    geometry: {
-                        type: 'Point',
-                        coordinates: file._geoloc
-                    },
-                    properties: {
-                        title: file._filename,
-                        icon: 'marker-15',
-                        description: '#seaitrise from Kinvey',
-                        photo_page_url: file._downloadURL,
-                        photo_id: file._id
-                    }
-
-                });
-                   
-                //console.log(markerJSON)
-                return '<tr>\n'
-                  + '<td>' + file._filename + '</td>\n'
-                  + '<td>' + file._geoloc + '</td>\n'
-                  + '<td>' + file.mimeType + '</td>\n'
-                  + '<td>' + file._public + '</td>\n'
-                  + '<td><a target="_blank" href="' + file._downloadURL + '" target="_blank">Download URL</a></td>\n'
-                  + '</tr>';
-                  
-            });
-            //console.log(markerJSON)
-
-            //add the markers to the map
-            //markerJSON.features.forEach(function (marker) {
-            //    // create a HTML element for each feature
-            //    var el = document.createElement('div');
-            //    el.className = 'marker';
-
-            //    // make a marker for each feature and add to the map
-            //    //console.log(marker)
-            //    new mapboxgl.Marker(el)
-            //    .setLngLat(marker.geometry.coordinates)
-            //    .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-            //    .setHTML('<p align="center"><a href="' + marker.properties.photo_page_url + '">' + marker.properties.title + '</a></p>' +
-            //            '<p align="center">' + marker.properties.description + '</p>'))
-            //    .addTo(markerMap);
-            //    //console.log(marker.properties)
-            //    });
-
-            // Create the table
-            var html = '<table class="table table-striped">\n'
-              + '<thead>\n'
-              + '<tr>\n'
-              + '<th>Title</th>\n'
-              + '<th>Lon,Lat</th>\n'
-              + '<th>MIME Type</th>\n'
-              + '<th>Public</th>\n'
-              + '<th>Url</th>\n'
-              + '</tr>\n'
-              + '</thead>\n'
-              + '<tbody>\n'
-              + rows.join('')
-              + '</tbody>\n'
-              + '</table>\n';
-
-
-
-            // Add the html to the page
-            $(selector).html(html);
-            //return markerJSON;
-        }
-
-        //search_coord = [-122.0, 47.0];
-        //radius = 100;
-        // create query
-        var query = new Kinvey.Query();
-        //query.near('_geoloc', search_coord,radius)
-        query.withinBox('_geoloc', bbox_ll,bbox_ur);
-
-        // Load the files
-        //Kinvey.Files.find()
-        Kinvey.Files.find(query)
-          .then(function (files) {
-              //console.log(markerJSON)
-              return markerJSON, renderTable(files, '#files-table')
-          });
-    },
-
-    uploadFile: function(event) {
-      // Prevent the form from being submitted
-      event.preventDefault();
-
-      // Remove the upload-success and upload-error
-      $('#upload-success').hide(0);
-      $('#upload-error').hide(0);
-      $('#exif-data').hide(0);
-
-      // Get entered values
-      var file = $('#file')[0].files[0];
-
-      if (file) {
-        var filename = $('#filename').val();
-        var public = document.getElementById('public').checked;
-        var acloptions_json = {};
-        filename = filename || filename !== '' ? filename : file.name;
-
-          // this is where I think the EXIF scrape can come for uploading to Kinvey. 
-          // Really belongs as a pre-save file hook on Kinvey, though.
-
-        // Show progress
-
-        $('#upload-progress').show(0);
-
-        
-        //console.log($('#user-location-lat')[0].textContent);
-        //console.log($('#user-location-lng'));
-
-        var file_geoloc = [parseFloat($('#user-location-lng')[0].textContent), parseFloat($('#user-location-lat')[0].textContent)];
-        //console.log(file_geoloc);
-        
-        if (!public) {
-            console.log('PRIVATE!!');
-            acloptions_json = { 'gr': false };
-        } else {
-            acloptions_json = { 'gr': true };
-        };
-        // Upload the file
-        Kinvey.Files.upload(file, { '_acl': acloptions_json,'_geoloc': file_geoloc, filename: filename, public: public }, { timeout: 10 * 60 * 1000 })
-          .then(function() {
-            // Hide progress
-            $('#upload-progress').hide(0);
-
-            // Show success message
-            $('#upload-success').html('<p>File uploaded!</p>').show(0);
-
-          })
-           
-          .catch(function(error) {
-            // Hide progress
-            $('#upload-progress').hide(0);
-
-            // Show error message
-            $('#upload-error').html('<p>' + error.message + '</p>').show(0);
-          });
+      var file_geoloc = [parseFloat($('#user-location-lng')[0].textContent), parseFloat($('#user-location-lat')[0].textContent)];
+      //console.log(file_geoloc);
+      
+      if (!public) {
+          console.log('PRIVATE!!');
+          acloptions_json = { 'gr': false };
       } else {
-        // Hide progress
-        $('#upload-progress').hide(0);
-
-        // Show error message
-        $('#upload-error').html('<p>Please select a file to upload.</p>').show(0);
-      }
-    },
-
-    saveProfile: function(event) {
-      // Get the active user
-      var activeUser = Kinvey.User.getActiveUser();
-
-      // Prevent the form from being submitted
-      event.preventDefault();
-
-      // Remove the profile-success and profile-error
-      $('#profile-success').hide(0);
-      $('#profile-error').hide(0);
-
-      // Get entered values
-      var data = {
-        username: $('#username').val(),
-        firstname: $('#firstname').val(),
-        lastname: $('#lastname').val()
+          acloptions_json = { 'gr': true };
       };
-
-      // Login to Kinvey
-      activeUser.update(data)
+      // Upload the file
+      Kinvey.Files.upload(file, { '_acl': acloptions_json,'_geoloc': file_geoloc, filename: filename, public: public }, { timeout: 10 * 60 * 1000 })
         .then(function() {
-          $('#profile-success').html('<p>Profile updated!</p>').show(0);
+          // Hide progress
+          $('#upload-progress').hide(0);
+
+          // Show success message
+          $('#upload-success').html('<p>File uploaded!</p>').show(0);
+
         })
+          
         .catch(function(error) {
-          $('#profile-error').html('<p>' + error.message + '</p>').show(0);
+          // Hide progress
+          $('#upload-progress').hide(0);
+
+          // Show error message
+          $('#upload-error').html('<p>' + error.message + '</p>').show(0);
         });
+    } else {
+      // Hide progress
+      $('#upload-progress').hide(0);
+
+      // Show error message
+      $('#upload-error').html('<p>Please select a file to upload.</p>').show(0);
     }
+  }
 };
 
 // Authorized Hrefs
