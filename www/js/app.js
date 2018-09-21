eb346df7c191f2bdb6815a9b28078c3ce4f16046
Christian P. Sarason <cpsarason@gmail.com>
2018-09-21 11:11:34 -0700

    Refactoring of methods into app.js and removal of old HTML pages

--- www/js/app.js
+++ www/js/app.js
@@ -14,7 +14,7 @@ var app = {
     '#profile-form submit': 'saveProfile'
   },
 
-  // Google Sheet API Stuff
+  // Google Sheet things that need identify the sheet we're pulling from
 
   googleSheetSetup: function googleSheetSetup() {
   
@@ -43,198 +43,230 @@ var app = {
     return gSheetJSON;
   },
 
+  
+// this was adapted from https://developers.google.com/sheets/api/quickstart/js#step_1_turn_on_the_api_name
+// for orcaSound use case
+  readObs: function readObs() {
 
-  // /**
-  //  *  On load, called to load the auth2 library and API client library.
-  //  */
-  // // *****SECTION: GOOGLE AUTH*******
-  // // TODO: This section should be moved to app.js
-
-  // handleClientLoad: function handleClientLoad() {
-  //   gapi.load('client:auth2', initClient);
-  // },
-
-  // /**
-  //  *  Initializes the API client library and sets up sign-in state
-  //  *  listeners.
-  //  */
-  // function initClient() {
-  //   gapi.client.init({
-  //     apiKey: API_KEY,
-  //     clientId: CLIENT_ID,
-  //     discoveryDocs: DISCOVERY_DOCS,
-  //     scope: SCOPES
-  //   }).then(function () {
-  //     // Listen for sign-in state changes.
-  //     gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-
-  //     // Handle the initial sign-in state.
-  //     updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-  //     authorizeButton.onclick = handleAuthClick;
-  //     signoutButton.onclick = handleSignoutClick;
-  //   });
-  // }
-
-  // /**
-  //  *  Called when the signed in status changes, to update the UI
-  //  *  appropriately. After a sign-in, the API is called.
-  //  */
-  // function updateSigninStatus(isSignedIn) {
-  //   if (isSignedIn) {
-  //     authorizeButton.style.display = 'none';
-  //     signoutButton.style.display = 'block';
-  //     listObs();
-  //   } else {
-  //     authorizeButton.style.display = 'block';
-  //     signoutButton.style.display = 'none';
-  //   }
-  // }
-
-  // /**
-  //  *  Sign in the user upon button click.
-  //  */
-  // function handleAuthClick(event) {
-  //   gapi.auth2.getAuthInstance().signIn();
-  // }
-
-  // /**
-  //  *  Sign out the user upon button click.
-  //  */
-  // function handleSignoutClick(event) {
-  //   gapi.auth2.getAuthInstance().signOut();
-  // }
-
-  // /**
-  //  * Append a pre element to the body containing the given message
-  //  * as its text node. Used to display the results of the API call.
-  //  *
-  //  * @param {string} message Text to be placed in pre element.
-  //  */
-  // function appendPre(message) {
-  //   var pre = document.getElementById('obsListing');
-  //   var textContent = document.createTextNode(message + '\n');
-  //   pre.appendChild(textContent);
-  // }
-  // // ***** END GOOGLE AUTH *******
-
-
-  authorizeSheet: function () {
-    console.log('stub of authorizing to google sheet');
-  },
-
-
-  loadTideStations: function (dataStoreType) {
-      console.log('in loadTideStations');
-      // Render the table
-      function renderTable(stations, selector) {
-          // Default stations to an empty array
-          stations = stations || [];
-
-          // Create the rows
-          var rows = stations.map(function (station) {
-              return '<tr>\n'
-                + '<td>' + station.name + '</td>\n'
-                + '<td>' + station.id + '</td>\n'
-                + '<td>' + station.lat + '</td>\n'
-                + '<td>' + station.lng + '</td>\n'
-                + '<td>' + (station.reference_id || '') + '</td>\n'
-                + '</tr>';
-          });
-
-          // Create the table
-          var html = '<table class="table table-striped">\n'
-            + '<thead>\n'
-            + '<tr>\n'
-            + '<th>Station Name</th>\n'
-            + '<th>ID</th>\n'
-            + '<th>Latitude</th>\n'
-            + '<th>Longitude</th>\n'
-            + '<th>Reference ID</th>\n'
-            + '</tr>\n'
-            + '</thead>\n'
-            + '<tbody>\n'
-            + rows.join('')
-            + '</tbody>\n'
-            + '</table>\n';
-
-          // Add the html to the page
-          $(selector).html(html);
-      }
-
-      // Load the stations
-      var store = Kinvey.DataStore.collection('NOAAStations', dataStoreType);
-      store.find()
-        .subscribe(function (stations) {
-            return renderTable(stations, '#NOAAStations-table')
-        });
-  },
-
-  uploadFile: function(event) {
-    // stub for uploading files to google sheet?? 
-    // Prevent the form from being submitted
-    event.preventDefault();
-
-    // Remove the upload-success and upload-error
-    $('#upload-success').hide(0);
-    $('#upload-error').hide(0);
-    $('#exif-data').hide(0);
-
-    // Get entered values
-    var file = $('#file')[0].files[0];
-
-    if (file) {
-      var filename = $('#filename').val();
-      var public = document.getElementById('public').checked;
-      var acloptions_json = {};
-      filename = filename || filename !== '' ? filename : file.name;
-
-        // this is where I think the EXIF scrape can come for uploading to Kinvey. 
-        // Really belongs as a pre-save file hook on Kinvey, though.
-
-      // Show progress
-
-      $('#upload-progress').show(0);
-
-      
-      //console.log($('#user-location-lat')[0].textContent);
-      //console.log($('#user-location-lng'));
-
-      var file_geoloc = [parseFloat($('#user-location-lng')[0].textContent), parseFloat($('#user-location-lat')[0].textContent)];
-      //console.log(file_geoloc);
-      
-      if (!public) {
-          console.log('PRIVATE!!');
-          acloptions_json = { 'gr': false };
-      } else {
-          acloptions_json = { 'gr': true };
+    var gsheetVerifiedObs = {
+        "type": "FeatureCollection",
+        "features": [{
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": []
+            },
+            "properties": {
+                "title": "MarkerClick",
+                //"icon": "marker",
+                //"fill-color": "#3887be"
+            }
+        }]
       };
-      // Upload the file
-      Kinvey.Files.upload(file, { '_acl': acloptions_json,'_geoloc': file_geoloc, filename: filename, public: public }, { timeout: 10 * 60 * 1000 })
-        .then(function() {
-          // Hide progress
-          $('#upload-progress').hide(0);
+  
+      var gsheetRawObs = {
+          "type": "FeatureCollection",
+          "features": [{
+              "type": "Feature",
+              "geometry": {
+                  "type": "Point",
+                  "coordinates": []
+              },
+              "properties": {
+                  "title": "MarkerClick",
+                  //"icon": "marker",
+                  //"fill-color": "#3887be"
+              }
+          }]
+        };
+  
+    //read in obs from the gSheet passed to this function
 
-          // Show success message
-          $('#upload-success').html('<p>File uploaded!</p>').show(0);
+    gapi.client.sheets.spreadsheets.values.get({
+      spreadsheetId: gSheetJSON.WORKBOOKID,
+      range: gSheetJSON.VISUALSHEET + '!' + gSheetJSON.RANGE,
+    }).then(function(response) {
+      var range = response.result;
+      if (range.values.length > 0) {
+        //console.log('Time, Type, User, Latitude, Longitude, Acoustic Intensity, Verified Intensity');
+        for (i = 0; i < range.values.length; i++) {
+          var row = range.values[i];
+          // Print columns A through D, which correspond to indices 0-3.
+          //console.log(row);
+          //console.log(row[7]);
+          if (row[7] === "yes" || row[7] === "Yes") {
+            var newObs = {
+              "type": "Feature",
+              "geometry": {
+                  "type": "Point",
+                  "coordinates": [parseFloat(row[5]),parseFloat(row[6])]
+              },
+              "properties": {
+                  "title": "Verified Sighting",
+                  "time": row[4],
+                  "user": row[3],
+                  "note": row[8]
+              }
+            }
+            gsheetVerifiedObs.features.push(newObs);
+            //console.log(newObs);
+          }
+          else if  (row[2] === "visual") {
+            var newRawObs = {
+              "type": "Feature",
+              "geometry": {
+                  "type": "Point",
+                  "coordinates": [parseFloat(row[5]),parseFloat(row[6])]
+              },
+              "properties": {
+                  "title": "Unverified Sighting",
+                  "observation time": row[4],
+                  "user": row[3],
+                  "note": row[8]
+              }
+            }
+            gsheetRawObs.features.push(newRawObs);
+            //console.log(gsheetVerifiedObs);
+            //console.log(newObs);
+          }
+        }
+  
+      } else {
+        console.log('No data found.');
+      }
+    }, function(response) {
+      console.log('Error: ' + response.result.error.message);
+    });
+    return [gsheetVerifiedObs, gsheetRawObs];
+  },
 
-        })
-          
-        .catch(function(error) {
-          // Hide progress
-          $('#upload-progress').hide(0);
-
-          // Show error message
-          $('#upload-error').html('<p>' + error.message + '</p>').show(0);
-        });
-    } else {
-      // Hide progress
-      $('#upload-progress').hide(0);
-
-      // Show error message
-      $('#upload-error').html('<p>Please select a file to upload.</p>').show(0);
+  writeObs: function writeObs(obs) {
+    //console.log(obs);
+    //console.log(JSON.stringify(obs));
+  
+    // test to make sure feature has an associated timestamp before adding it
+    if (obs.properties.timestamp) {
+      //console.log('obs has time parameter: ' + obs.properties.time);
+  
+      //TODO: this user from oauth is badly hardwired. need to figure out a better
+      //      way to id users
+      //TODO: add hooks here for other observation types, categories
+      //
+      //     The string format for entered dates should be: YYYY-MM-DDTHH:mm:ss.sssZ, where:
+      //
+          // YYYY-MM-DD – is the date: year-month-day.
+          // The character "T" is used as the delimiter.
+          // HH:mm:ss.sss – is the time: hours, minutes, seconds and milliseconds.
+          // The optional 'Z' part denotes the time zone in the format +-hh:mm. A
+          // single letter Z that would mean UTC+0.
+  
+      var values = [
+        [
+          obs.properties.timestamp,
+          obs.properties.humantime,
+          "visual",
+          gapi.auth2.getAuthInstance().currentUser.Ab.w3.U3,
+          "Enter <YYYY-MM-DDTHH:MM:SSZZZ>",
+          obs.geometry.coordinates[0],
+          obs.geometry.coordinates[1]
+        ],
+        // Additional rows ...
+      ];
+      var body = {
+        values: values
+      };
+  
+      //TODO: update to bulk update, rows are being dropped 5/31/2018
+      gapi.client.sheets.spreadsheets.values.append({
+        spreadsheetId: gSheetJSON.WORKBOOKID,
+        range: gSheetJSON.VISUALSHEET + '!' + gSheetJSON.RANGE,
+         valueInputOption: 'USER_ENTERED',
+         insertDataOption: 'INSERT_ROWS',
+         resource: body
+      }).then((response) => {
+        var result = response.result;
+        //console.log(response);
+        //console.log(`${result.updatedCells} cells updated.`);
+      });
     }
+  },
+
+} 
+
+// this was adapted from https://developers.google.com/sheets/api/quickstart/js#step_1_turn_on_the_api_name
+// for orcaSound use case
+/**
+ *  On load, called to load the auth2 library and API client library.
+ */
+// *****SECTION: GOOGLE AUTH*******
+// TODO: This section should be moved to app.js
+
+function handleClientLoad() {
+  gapi.load('client:auth2', initClient);
+}
+
+/**
+ *  Initializes the API client library and sets up sign-in state
+ *  listeners.
+ */
+function initClient() {
+  gapi.client.init({
+    apiKey: gSheetJSON.API_KEY,
+    clientId: gSheetJSON.CLIENT_ID,
+    discoveryDocs: gSheetJSON.DISCOVERY_DOCS,
+    scope: gSheetJSON.SCOPES
+  }).then(function () {
+    // Listen for sign-in state changes.
+    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+    // Handle the initial sign-in state.
+    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    authorizeButton.onclick = handleAuthClick;
+    signoutButton.onclick = handleSignoutClick;
+  });
+}
+
+/**
+ *  Called when the signed in status changes, to update the UI
+ *  appropriately. After a sign-in, the API is called.
+ */
+function updateSigninStatus(isSignedIn) {
+  if (isSignedIn) {
+    authorizeButton.style.display = 'none';
+    signoutButton.style.display = 'block';
+  } else {
+    authorizeButton.style.display = 'block';
+    signoutButton.style.display = 'none';
   }
-};
+}
+
+/**
+ *  Sign in the user upon button click.
+ */
+function handleAuthClick(event) {
+  gapi.auth2.getAuthInstance().signIn();
+}
+
+/**
+ *  Sign out the user upon button click.
+ */
+function handleSignoutClick(event) {
+  gapi.auth2.getAuthInstance().signOut();
+}
+
+/**
+ * Append a pre element to the body containing the given message
+ * as its text node. Used to display the results of the API call.
+ *
+ * @param {string} message Text to be placed in pre element.
+ */
+function appendPre(message) {
+  var pre = document.getElementById('obsListing');
+  var textContent = document.createTextNode(message + '\n');
+  pre.appendChild(textContent);
+}
+// ***** END GOOGLE AUTH *******
+
 
 // Authorized Hrefs
 var authorizedHrefs = [
@@ -245,38 +277,4 @@ var authorizedHrefs = [
   '/profile.html',
   '/map-markers.html',
 ];
-
-// Bind events
-function bindEvents() {
-  return new RSVP.Promise(function(resolve) {
-    var elementEventKeys = Object.keys(app.events);
-    elementEventKeys.forEach(function(elementEventKey) {
-      var element = elementEventKey.split(' ')[0];
-      var event = elementEventKey.split(' ')[1];
-      $(element).on(event, app[app.events[elementEventKey]]);
-    });
-    resolve();
-  });
-}
-
-// Initialize Kinvey
-Kinvey.initialize({
-  appKey: 'kid_ryiIKffKZ',
-  appSecret: 'cdf8c00f516e4941a691eab1275ed2a6'
-})
-  .then(function(activeUser) {
-    if (!activeUser && authorizedHrefs.indexOf(location.pathname) !== -1) {
-        location.replace('/login.html');
-        //console.log(location.pathname);
-    } else {
-        return bindEvents();
-    }
-  })
-  .then(function() {
-      $(document).trigger('app.ready');
-  })
-  .catch(function(error) {
-    alert(error.message);
-    console.log(error);
-  });
 
